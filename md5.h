2f02a289621663c8f9ff85127562ddbcb1540bc3
Tomas Brazas <g00349242.gmit.ie>
2020-04-30 12:51:14 +0000

    Added defs/struct into header file

--- md5.h
+++ md5.h
@@ -1,4 +1,24 @@
 /* Tomas Brazas
  * G00349242
  * MD5 Header
- * /
+ */
+ 
+#ifndef _MD5_H
+#define _MD5_H
+
+#ifndef uint8
+#define uint8  unsigned char
+#endif
+
+#ifndef uint32
+#define uint32 unsigned long int
+#endif
+
+typedef struct
+{
+    uint32 total[2];
+    uint32 state[4];
+    uint8 buffer[64];
+}
+md5_context;
+#endif /* md5.h */
